10d263ccff4afd0a59e2aaf790364baeca3a5295
mick26 <mcullen26@gmail.com>
2015-02-03 08:20:28 +0000

    fix route error

--- public/js/app.js
+++ public/js/app.js
@@ -29,7 +29,7 @@ angular.module('myApp', ['ngRoute', 'ngMaterial', 'ngAnimate', 'ngSanitize', 'my
       })
       
       .otherwise({
-        redirectTo: '/login'
+        redirectTo: '/'
       })
 
 });
